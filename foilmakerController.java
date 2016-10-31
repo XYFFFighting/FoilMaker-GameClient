bfea1a34b6f811fbb8ae3856a0736622a2bc2bbf
XYFFFighting <wyqss1227@gmail.com>
2016-10-30 22:20:49 -0400

    Update foilmakerController.java

--- foilmakerController.java
+++ foilmakerController.java
@@ -1,8 +1,27 @@
 /**
  * Created by Yufei Xu on 10/22/2016.
  *implements network programming bits, application protocol
+ * users input
  */
-//wang
 public class foilmakerController {
+    public static void main(String[] args) {
+        foilmakerView f = new foilmakerView();
+//test GUI
+        f.setSize(300,500);
+        //f.setResizable(false);
+        //f.Login();
+        //f.Login2();
+        //f.JoinGame();
+        //f.StartnewGame();
+        //f.Waiting();
+        //f.Suggestionwords();
+        //f.pickoption();
+        f.receiveResults();
+        f.setVisible(true);
+
+    }
+
+
+
 
 }
