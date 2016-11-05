10e767c0895d848c94eb8e1ffb5ea83ebdcec400
wang2929 <wang2929@purdue.edu>
2016-11-05 17:49:45 -0400

    Update foilmakerView.java

--- foilmakerView.java
+++ foilmakerView.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by Yufei Xu on 10/22/2016.
