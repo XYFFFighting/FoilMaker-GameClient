6b50da289df9d603550e54d42d5711757f368071
XYFFFighting <wyqss1227@gmail.com>
2016-11-05 17:47:41 -0400

    Update foilmakerController.java

--- foilmakerController.java
+++ foilmakerController.java
@@ -1,14 +1,37 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by Yufei Xu on 10/22/2016.
  *implements network programming bits, application protocol
  * users input
  */
 public class foilmakerController {
-    public static void main(String[] args) {
+    static Socket socket;
+    public static void main(String[] args) throws IOException {
         foilmakerView f = new foilmakerView();
+
+        int serverPort;
+
+        String serverIP = "localhost";
+        serverPort = 5000;
+
+
+        try {
+            socket = new Socket(serverIP, serverPort);
+        } catch (IOException e) {
+            System.err.println("Error with IO:\n" + e.getMessage());
+            System.exit(3);
+
+
+        }
+        f.run();
+
 //test GUI
         //f.setSize(300,500);
-        f.run();
+
         //f.Login();
         //f.Login2();
         //f.JoinGame();
@@ -18,10 +41,9 @@ public class foilmakerController {
         //f.pickoption();
         //f.receiveResults();
         //f.setVisible(true);
-
     }
-
-
-
-
 }
+
+
+
+
