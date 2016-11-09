4488cb38b5aaa889bc119d6c5d28e2a8b06a2997
XYFFFighting <wyqss1227@gmail.com>
2016-11-09 18:51:07 -0500

    Update foilmakerClient.java

--- foilmakerClient.java
+++ foilmakerClient.java
@@ -1,6 +1,8 @@
+import javax.swing.*;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.Random;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,53 +14,55 @@ import java.util.Scanner;
  */
 public class foilmakerClient {
     public static String reply;
+    // public static String reply2;
 
-    public static void sendmessage(String inputLine) throws  IOException {
-
-        Socket socket =foilmakerController.socket;
+    public static void sendmessage(String inputLine) throws IOException {
+        Socket socket = foilmakerController.socket;
+        /*
         PrintWriter outToServer = null;
         BufferedReader inFromServer = null;
 
         Scanner scanner = new Scanner(System.in);
 
 
+        // Connect to server
+        //System.out.println("Enter the server port");
+        //serverPort = scanner.nextInt();
+        //socket = new Socket(serverIP, serverPort);
+        OutputStream os = socket.getOutputStream();
+        outToServer = new PrintWriter(os, true);
+        InputStream is = socket.getInputStream();
+        inFromServer = new BufferedReader(new InputStreamReader(is));
+        outToServer.println(inputLine);
+        */
+        PrintWriter outToServer = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        outToServer.println(inputLine);
+        socket.setSoTimeout(1);
+        //reply=inFromServer.readLine();
+        reply = null;
+        while (reply == null) {
+            try {
+                reply = inFromServer.readLine();
+                System.out.println(reply);
+            } catch (SocketTimeoutException e) {
 
-        try {
-            // Connect to server
-            //System.out.println("Enter the server port");
-            //serverPort = scanner.nextInt();
-            //socket = new Socket(serverIP, serverPort);
-            OutputStream os = socket.getOutputStream();
-            outToServer = new PrintWriter(os);
-            InputStream is = socket.getInputStream();
-            inFromServer = new BufferedReader(new InputStreamReader(is));
-            outToServer.println(inputLine);
-            outToServer.flush();
-            //socket.shutdownOutput();
-            reply=inFromServer.readLine();
-
-                if (!((reply==null))){
-                System.out.println("Recieved mesaage："+reply);
             }
-        } catch (MalformedURLException e) {
-                System.err.println("Error with URL:\n" + e.getMessage());
-                System.exit(2);
-            } catch (IOException e) {
-                System.err.println("Error with IO:\n" + e.getMessage());
-                System.exit(3);
-            } /*
-            finally {
-                if(socket != null)
-                    socket.close();
-                if(outToServer != null)
-                    outToServer.close();
-                if(inFromServer != null)
-                    inFromServer.close();
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+        socket.setSoTimeout(0);
+
+/*            if((reply=inFromServer.readLine())!=null) {
+                System.out.println("Recieved mesaage：" + reply);
+
 
             }
             */
+
     }
-
-
-
 }
